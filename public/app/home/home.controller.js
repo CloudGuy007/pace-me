3c256d6fba8c1dab2b9095540c3149963bb4bcc7
Sergio Prada <chechopradaster@gmail.com>
2016-04-19 22:21:02 -0700

    home routes

---
+++ public/app/home/home.controller.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var app = angular.module('paceMeApp');
+
+app.controller('homeCtrl', function(){
+
+	console.log('homeCtrl');
+
+
+})
---
+++ public/app/module.states.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+var app = angular.module('paceMeApp')
+
+app.config(function($stateProvider, $urlRouterProvider){
+	$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: '/app/home/home.html',
+			controller: 'homeCtrl'
+		});
+
+	$urlRouterProvider.otherwise('/');
+
+
+
+
+})
