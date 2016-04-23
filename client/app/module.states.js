50518e1d5b918d4c41c43aa8602130f1a034fb46
Deanna Shih <deanna.x.shih@gmail.com>
2016-04-23 15:56:29 -0700

    modify home

--- client/app/module.states.js
+++ client/app/module.states.js
@@ -59,9 +59,8 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider){
 	});
 });
 
-// app.run(function($stormpath){
-//   $stormpath.uiRouter({
-//     loginState: 'list',
-//     defaultPostLoginState: 'profile'
-//   });
-// });
+app.run(function($stormpath){
+  $stormpath.uiRouter({
+    defaultPostLoginState: 'profile'
+  });
+});
