1b3b9a5de3168e8fe84267cf3f9fda3cf7214e6b
andrewcbass <andrew.c.bass@gmail.com>
2016-04-27 11:38:14 -0700

    reformat put and post

--- users/users.js
+++ users/users.js
@@ -90,28 +90,67 @@ router.put('/matches', function(req, res, next){
 
 //can be used to create or edit user
 router.post('/me',function(req, res, next){
-  client.hmset(`user:${req.body.email}`, "firstName", `${req.body.firstName}`, "lastName", `${req.body.lastName}`,
-    "email", `${req.body.email}`, "photo", `${req.body.photo}`, "age", `${req.body.age}`, "gender", `${req.body.gender}`,
-    "phone", `${req.body.phone}`, "zipCode", `${req.body.zipCode}`, "about", `${req.body.about}`, "registered", `${req.body.registered}`,
-    "wklyMileage", `${req.body.wklyMileage}`, "runEvent", `${req.body.runEvent}`, "sixtyM", `${req.body.sixtyM}`,
-    "oneHundM", `${req.body.oneHundM}`,"twoHundM", `${req.body.twoHundM}`, "fourHundM", `${req.body.fourHundM}`,
-    "onemiPR", `${req.body.onemiPR}`, "fivekPR", `${req.body.fivekPR}`, "tenkPR", `${req.body.tenkPR}`, "halfPR", `${req.body.halfPR}`,
-    "marathonPR", `${req.body.marathonPR}`, "milePace", `${req.body.milePace}`, "longestDistRun", `${req.body.longestDistRun}`,
-    "longitude", `${req.body.longitude}`, "latitude", `${req.body.latitude}`);
+  client.hmset(`user:${req.body.email}`,
+    "firstName", `${req.body.firstName}`,
+    "lastName", `${req.body.lastName}`,
+    "email", `${req.body.email}`,
+    "photo", `${req.body.photo}`,
+    "age", `${req.body.age}`,
+    "gender", `${req.body.gender}`,
+    "phone", `${req.body.phone}`,
+    "zipCode", `${req.body.zipCode}`,
+    "about", `${req.body.about}`,
+    "registered", `${req.body.registered}`,
+    "wklyMileage", `${req.body.wklyMileage}`,
+    "runEvent", `${req.body.runEvent}`,
+    "sixtyM", `${req.body.sixtyM}`,
+    "oneHundM", `${req.body.oneHundM}`,
+    "twoHundM", `${req.body.twoHundM}`,
+    "fourHundM", `${req.body.fourHundM}`,
+    "onemiPR", `${req.body.onemiPR}`,
+    "fivekPR", `${req.body.fivekPR}`,
+    "tenkPR", `${req.body.tenkPR}`,
+    "halfPR", `${req.body.halfPR}`,
+    "marathonPR", `${req.body.marathonPR}`,
+    "milePace", `${req.body.milePace}`,
+    "longestDistRun", `${req.body.longestDistRun}`,
+    "longitude", `${req.body.longitude}`,
+    "latitude", `${req.body.latitude}`);
+  //add the user to the UserLocs geodata
   client.geoadd("UserLocs", req.body.longitude, req.body.latitude, `user:${req.body.email}`)
 });
 	res.send('User Created!');
 });
 
+//edit user
 router.put('/:email/me',function(req, res, next){
-  client.hmset(`user:${req.params.email}`, "firstName", `${req.body.firstName}`, "lastName", `${req.body.lastName}`,
-    "email", `${req.body.email}`, "photo", `${req.body.photo}`, "age", `${req.body.age}`, "gender", `${req.body.gender}`,
-    "phone", `${req.body.phone}`, "zipCode", `${req.body.zipCode}`, "about", `${req.body.about}`, "registered", `${req.body.registered}`,
-    "wklyMileage", `${req.body.wklyMileage}`, "runEvent", `${req.body.runEvent}`, "sixtyM", `${req.body.sixtyM}`,
-    "oneHundM", `${req.body.oneHundM}`,"twoHundM", `${req.body.twoHundM}`, "fourHundM", `${req.body.fourHundM}`,
-    "onemiPR", `${req.body.onemiPR}`, "fivekPR", `${req.body.fivekPR}`, "tenkPR", `${req.body.tenkPR}`, "halfPR", `${req.body.halfPR}`,
-    "marathonPR", `${req.body.marathonPR}`, "milePace", `${req.body.milePace}`, "longestDistRun", `${req.body.longestDistRun}`,
-    "longitude", `${req.body.longitude}`, "latitude", `${req.body.latitude}`);
+  client.hmset(`user:${req.body.email}`,
+    "firstName", `${req.body.firstName}`,
+    "lastName", `${req.body.lastName}`,
+    "email", `${req.body.email}`,
+    "photo", `${req.body.photo}`,
+    "age", `${req.body.age}`,
+    "gender", `${req.body.gender}`,
+    "phone", `${req.body.phone}`,
+    "zipCode", `${req.body.zipCode}`,
+    "about", `${req.body.about}`,
+    "registered", `${req.body.registered}`,
+    "wklyMileage", `${req.body.wklyMileage}`,
+    "runEvent", `${req.body.runEvent}`,
+    "sixtyM", `${req.body.sixtyM}`,
+    "oneHundM", `${req.body.oneHundM}`,
+    "twoHundM", `${req.body.twoHundM}`,
+    "fourHundM", `${req.body.fourHundM}`,
+    "onemiPR", `${req.body.onemiPR}`,
+    "fivekPR", `${req.body.fivekPR}`,
+    "tenkPR", `${req.body.tenkPR}`,
+    "halfPR", `${req.body.halfPR}`,
+    "marathonPR", `${req.body.marathonPR}`,
+    "milePace", `${req.body.milePace}`,
+    "longestDistRun", `${req.body.longestDistRun}`,
+    "longitude", `${req.body.longitude}`,
+    "latitude", `${req.body.latitude}`);
+  //update the user to the UserLocs geodata
   client.geoadd("UserLocs", req.body.longitude, req.body.latitude, `user:${req.body.email}`)
 });
 	res.send('User Updated!');
