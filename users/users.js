86aed914e2151fd68b926936ea861a8ae4431edf
andrewcbass <andrew.c.bass@gmail.com>
2016-04-27 11:34:01 -0700

    update post

--- users/users.js
+++ users/users.js
@@ -91,13 +91,15 @@ router.put('/matches', function(req, res, next){
 //can be used to create or edit user
 router.post('/:id/me',function(req, res, next){
   client.hmset(`user:${req.body.email}`, "firstName", `${req.body.firstName}`, "lastName", `${req.body.lastName}`,
-    "email", `${req.body.email}`, "photo", `${req.body.picture}`, "age", `${req.body.age}`, "gender", `${req.body.gender}`,
+    "email", `${req.body.email}`, "photo", `${req.body.photo}`, "age", `${req.body.age}`, "gender", `${req.body.gender}`,
     "phone", `${req.body.phone}`, "zipCode", `${req.body.zipCode}`, "about", `${req.body.about}`, "registered", `${req.body.registered}`,
     "wklyMileage", `${req.body.wklyMileage}`, "runEvent", `${req.body.runEvent}`, "sixtyM", `${req.body.sixtyM}`,
-    "twoHundM", `${req.body.twoHundM}`, "fourHundM", `${req.body.fourHundM}`, "onemiPR", `${req.body.onemiPR}`,
-    "fivekPR", `${req.body.fivekPR}`, "tenkPR", `${req.body.tenkPR}`, "halfPR", `${req.body.halfPR}`, "marathonPR", `${req.body.marathonPR}`,
-    "milePace", `${req.body.milePace}`, "longitude", `${req.body.longitude}`, "latitude", `${req.body.latitude}`);
-  client.geoadd("UserLocs", req.body.longitude, req.body.latitude, `user:${req.body.email}`);
+    "oneHundM", `${req.body.oneHundM}`,"twoHundM", `${req.body.twoHundM}`, "fourHundM", `${req.body.fourHundM}`,
+    "onemiPR", `${req.body.onemiPR}`, "fivekPR", `${req.body.fivekPR}`, "tenkPR", `${req.body.tenkPR}`, "halfPR", `${req.body.halfPR}`,
+    "marathonPR", `${req.body.marathonPR}`, "milePace", `${req.body.milePace}`, "longestDistRun", `${req.body.longestDistRun}`,
+    "longitude", `${req.body.longitude}`, "latitude", `${req.body.latitude}`);
+  client.geoadd("UserLocs", req.body.longitude, req.body.latitude, `user:${req.body.email}`)
+});
 	res.send('User Created and Updated!');
 });
 
