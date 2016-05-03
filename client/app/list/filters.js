701df2514f1be8a7e2a42e5145454ea66589a6e9
Sean Yesmunt <sean.yesmunt@gmail.com>
2016-05-02 23:17:58 -0700

    pace diff filter

--- client/app/list/filters.js
+++ client/app/list/filters.js
@@ -8,20 +8,36 @@ app.filter('distance', function() {
     if (distance === 0){
       return "in your town";
     }
-
     return distance + " miles away";
 	}
 });
 
 app.filter('paceDiff', function() {
-	return function(arg1, arg2){
-		console.log("arg1", arg1);
-    console.log("arg2", arg2);
-    var diff = arg1-arg2;
-    if (diff > 0){
-      return '+' + diff + ' seconds';
+	return function(user, loggedIn){
+		var time = user - loggedIn;
+		var absTime = Math.abs(time);
+		var min = Math.floor(absTime / 60);
+		var seconds = absTime - min * 60;
+
+		if(seconds < 10) {
+			var diff = min + ':0' + seconds
+		} else {
+			var diff = min + ':' + seconds;
+		}
+    if (time > 0){
+      return '+ ' + diff;
     } else {
-      return diff + ' seconds';
+      return diff;
     }
+	}
+});
+
+app.filter('minutes2Seconds', function() {
+	return function(seconds){
+		if(isNaN(seconds) || seconds < 1){
+			return seconds;
+		} else {
+			return new Date(1970, 0, 1).setSeconds(seconds);
+		}
 	}
 });
