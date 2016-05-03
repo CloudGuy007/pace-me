0102f1d5fd0d38bb5ba1d28515fe7e5580e4d220
Sean Yesmunt <sean.yesmunt@gmail.com>
2016-05-02 23:35:29 -0700

    modularize filters

--- client/app/list/list.filters.js
+++ client/app/list/list.filters.js
@@ -31,13 +31,3 @@ app.filter('paceDiff', function() {
     }
 	}
 });
-
-app.filter('minutes2Seconds', function() {
-	return function(seconds){
-		if(isNaN(seconds) || seconds < 1){
-			return seconds;
-		} else {
-			return new Date(1970, 0, 1).setSeconds(seconds);
-		}
-	}
-});
