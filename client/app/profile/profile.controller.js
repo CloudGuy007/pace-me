cde96b4054e7fbbf57d656851508021857a16358
Deanna Shih <deanna.x.shih@gmail.com>
2016-05-06 19:15:30 -0700

    fine tuning

--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -3,9 +3,9 @@
     .module('paceMeApp')
     .controller('profileCtrl', profileCtrl)
 
-    profileCtrl.$inject = ['$scope', '$state', '$uibModal', '$log', 'ListService', 'ProfileService']
+    profileCtrl.$inject = ['$scope', '$state', '$uibModal', 'ListService', 'ProfileService']
 
-    function profileCtrl($scope, $state, $uibModal, $log, ListService, ProfileService) {
+    function profileCtrl($scope, $state, $uibModal, ListService, ProfileService) {
 
       ProfileService.getProfile($state.params.id)
         .then(function(res) {
@@ -29,15 +29,12 @@
         });
       };
 
-      $scope.toggleAnimation = function() {
-        $scope.animationsEnabled = !$scope.animationsEnabled;
-      };
-
       $scope.editRun = function(runner) {
         $scope.runnerEdit = angular.copy(runner);
         var mile = $scope.runnerEdit.milePace;
         var min = Math.floor(mile / 60);
         var sec = mile - min * 60;
+        if (sec === 0) sec = '00';
         $scope.runnerEdit.milePace = `${min}:${sec}`;
         for (let key in $scope.runnerEdit) {
           if ($scope.runnerEdit[key] === 'undefined') $scope.runnerEdit[key] = '';
@@ -49,9 +46,6 @@
           controller: 'ModalInstanceCtrl',
           size: 0,
           scope: $scope
-        });
-        modalInstance.result.then(function() {
-          $log.info('Modal dismissed at: ' + new Date());
         });
       }
 
